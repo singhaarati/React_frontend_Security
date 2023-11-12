89adba581a511a3a6f30996751213ddaad227531
singhaarati <aarati5737@gmail.com>
2023-11-12 13:11:00 +0545

    frontend of web application for security started

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Dashboard from './components/Dashboard';
+import Login from './components/Login';
+import { RequireAuth } from './utils/RequireAuth';
+import { AuthProvider } from './utils/authContext';
+
+import AddDestination from './components/AddDestination';
+import Booking from './components/Booking';
+import AboutUs from './components/Dashboard/AboutUs';
+import DetailDestination from './components/DetailDestination';
+import MyBookingsList from './components/MyBookings';
+import Signup from './components/Signup';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      {/* <h1 className='text-center'>Trip Planner</h1> */}
+      <AuthProvider>
+        <Routes>
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Signup />} />
+          <Route path='/' element={<Dashboard />} />
+          <Route path='/aboutus' element={<AboutUs />} />
+          <Route path='/destination/:id' element={<DetailDestination />} />
+          <Route path='/bookings/:id' element={<RequireAuth><Booking /></RequireAuth>} />
+          <Route path='/MyBookings' element={<MyBookingsList />} />
+          <Route path='/AddDestination' element={<AddDestination />} />
+        </Routes>
+      </AuthProvider>
     </div>
   );
 }
 
 export default App;
+
